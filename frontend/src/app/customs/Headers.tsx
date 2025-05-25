b3274ffc83d14be5aa62b874a75103df5167e72e
avhinaw <avhinaaw@gmail.com>
2025-05-25 16:32:27 +0530

    updated navbar, globals css and added react icons

--- frontend/src/app/customs/Headers.tsx
+++ frontend/src/app/customs/Headers.tsx
@@ -1,24 +1,54 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { IoBagHandleOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+import { IoSearchOutline } from "react-icons/io5";
 export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <nav className='text-white p-5 flex items-center justify-end gap-10 text-2xl'>
-      <Link style={linkStyle(pathname === '/')} href="/">Home</Link>
-      <Link style={linkStyle(pathname === '/products')} href="/products">Products</Link>
-      <Link style={linkStyle(pathname === '/checkout')} href="/checkout">Checkout</Link>
-      <Link style={linkStyle(pathname === '/orders')} href="/orders">Orders</Link>
-      <Link style={linkStyle(pathname === '/customers')} href="/customers">Customers</Link>
+    <nav className="py-3 px-5 mb-2 border-b-1 border-gray-300 flex justify-between w-screen">
+      <div className="flex gap-5 items-center">
+        <Link style={linkStyle(pathname === "/")} href="/">
+          <img src="/logo.svg" alt="" />
+        </Link>
+        <Link style={linkStyle(pathname === "/products")} href="/products">
+          Products
+        </Link>
+
+        <Link style={linkStyle(pathname === "/orders")} href="/orders">
+          Orders
+        </Link>
+        <Link style={linkStyle(pathname === "/customers")} href="/customers">
+          Customers
+        </Link>
+      </div>
+      <div className="flex gap-4">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search"
+            className="rounded-xl py-2 px-8 bg-gray-200 text-[#e92933]"
+          />
+          <div className="absolute top-3 right-58 text-[#e92933]">
+            <IoSearchOutline />
+          </div>
+        </div>
+        <div className="bg-gray-200 w-10 h-10 rounded-full flex items-center justify-center text-lg">
+          <FaRegHeart />
+        </div>
+        <Link style={linkStyle(pathname === "/checkout")} href="/checkout">
+          <div className="bg-gray-200 w-10 h-10 rounded-full flex items-center justify-center text-lg">
+            <IoBagHandleOutline />
+          </div>
+        </Link>
+      </div>
     </nav>
   );
 }
 
-
 const linkStyle = (active: boolean): React.CSSProperties => ({
-  fontWeight: active ? 'bold' : 'normal',
-  textDecoration: active ? 'underline' : 'none',
+  color: active ? "#e92933" : "black",
 });
